e20f930c6c6efd573eaea2d46e49f18fc5943226
Daiber Gonzalez <killermu94@gmail.com>
2021-05-11 18:09:31 -0500

    Improve code according to PR comments

---
+++ wbooks/src/app/components/AppNavigator/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import LibraryScreen from '@screens/Library';
+import BookDetailScreen from '@screens/BookDetail';
+import { ROUTES } from '@constants/routes';
+
+const LibraryStack = createStackNavigator();
+
+const AppNavigator = () => {
+  return (
+    <LibraryStack.Navigator initialRouteName={ROUTES.library}>
+      <LibraryStack.Screen name={ROUTES.library} component={LibraryScreen} />
+      <LibraryStack.Screen name={ROUTES.bookDetail} component={BookDetailScreen} />
+    </LibraryStack.Navigator>
+  );
+};
+
+export default AppNavigator;
--- wbooks/src/app/index.tsx
+++ wbooks/src/app/index.tsx
@@ -1,19 +1,11 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import LibraryScreen from '@screens/Library';
-import BookDetailScreen from '@screens/BookDetail';
-import { ROUTES } from '@constants/routes';
-
-const LibraryStack = createStackNavigator();
+import AppNavigator from '@components/AppNavigator';
 
 const App = () => {
   return (
     <NavigationContainer>
-      <LibraryStack.Navigator initialRouteName={ROUTES.library}>
-        <LibraryStack.Screen name={ROUTES.library} component={LibraryScreen} />
-        <LibraryStack.Screen name={ROUTES.bookDetail} component={BookDetailScreen} />
-      </LibraryStack.Navigator>
+      <AppNavigator />
     </NavigationContainer>
   );
 };
