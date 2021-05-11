05457df26e826eef85ba78af62d1a1242733abbf
Daiber Gonzalez <killermu94@gmail.com>
2021-05-11 18:38:46 -0500

    Add statusbar

--- wbooks/src/app/index.tsx
+++ wbooks/src/app/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import AppNavigator from '@components/AppNavigator';
 
 const App = () => {
   return (
     <NavigationContainer>
+      <StatusBar barStyle="dark-content" />
       <AppNavigator />
     </NavigationContainer>
   );
