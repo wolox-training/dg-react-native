d9bc4e3898545799b0573182c17473960c0b2404
Daiber Gonzalez <killermu94@gmail.com>
2021-05-12 22:12:57 -0500

    Connect reactotron with redux

--- wbooks/src/redux/store.ts
+++ wbooks/src/redux/store.ts
@@ -1,5 +1,6 @@
 import { createStore } from 'redux';
+import Reactotron from '@config/reactotronConfig';
 
 import bookReducer from './book/reducer';
 
-export default createStore(bookReducer);
+export default createStore(bookReducer, Reactotron.createEnhancer());
