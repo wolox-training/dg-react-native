930f2e9d53293b80fe05171628d9960da1ff8792
Daiber Gonzalez <killermu94@gmail.com>
2021-05-13 12:19:04 -0500

    Create correct type to actions

--- wbooks/src/redux/book/reducer.ts
+++ wbooks/src/redux/book/reducer.ts
@@ -3,11 +3,15 @@ import { BookState } from '@interfaces/Book';
 
 import { actions } from './actions';
 
+interface Action {
+  type: string;
+}
+
 const initialState: BookState = {
   books: []
 };
 
-function bookReducer(state = initialState, action: any): BookState {
+function bookReducer(state = initialState, action: Action): BookState {
   switch (action.type) {
     case actions.GET_BOOKS:
       return { ...state, books: BOOKS_MOCK };
