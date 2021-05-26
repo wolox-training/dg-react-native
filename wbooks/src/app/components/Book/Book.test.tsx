0392b3ab942ff8aeb610d9896997faf03d8fdeed
Daiber Gonzalez <killermu94@gmail.com>
2021-05-26 16:00:33 -0500

    Add unit test to Book component

--- wbooks/src/app/components/Book/Book.test.tsx
+++ wbooks/src/app/components/Book/Book.test.tsx
@@ -1,11 +1,51 @@
 import React from 'react';
-import { render } from '@testing-library/react-native';
+import { fireEvent, render } from '@testing-library/react-native';
+import bookPlaceholder from '@assets/book/img_book_placeholder.png';
+import { ROUTES } from '@constants/routes';
 
-import Book from '.';
+import Book from './index';
+
+const mockedNavigate = jest.fn();
+
+jest.mock('@react-navigation/native', () => {
+  return {
+    useNavigation: () => ({
+      navigate: mockedNavigate
+    })
+  };
+});
 
 describe('Book test suits', () => {
+  const props = {
+    author: 'John Doe',
+    id: 1,
+    title: 'Tibia'
+  };
+
   it('Render correctly', () => {
-    const book = render(<Book author="" id={0} title="" />);
-    expect(book).toBeTruthy();
+    const book = render(<Book {...props} />);
+    expect(book).toMatchSnapshot();
+  });
+  it('Render with all props', () => {
+    const imageUrl = 'test image';
+    const { getByLabelText, getByText } = render(<Book {...props} imageUrl={imageUrl} />);
+    const image = getByLabelText('bookImage');
+    expect(getByLabelText('bookTitle')).toBeTruthy();
+    expect(getByText(props.title)).toBeTruthy();
+    expect(getByLabelText('bookAuthor')).toBeTruthy();
+    expect(getByText(props.author)).toBeTruthy();
+    expect(image).toBeTruthy();
+    expect(image.props.source).toEqual({ uri: imageUrl });
+  });
+  it('Render without imageUrl', () => {
+    const { getByLabelText } = render(<Book {...props} />);
+    const image = getByLabelText('bookImage');
+    expect(image.props.source).toEqual(bookPlaceholder);
+  });
+  it('Test onNavigate', () => {
+    const { getByLabelText } = render(<Book {...props} />);
+    const item = getByLabelText('bookItem');
+    fireEvent.press(item);
+    expect(mockedNavigate).toBeCalledWith(ROUTES.bookDetail);
   });
 });
